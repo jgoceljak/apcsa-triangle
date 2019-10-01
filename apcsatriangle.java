8a8020069e9da2e4e778c88641b6b91c2bfb47bb
jgoceljak <john.goceljak@gmail.com>
2019-10-01 08:59:25 -0400

    final

---
+++ apcsatriangle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class apcsatriangle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("\nEnter a height: ");
+        double height = in.nextDouble();
+
+        System.out.print("Enter a base: ");
+        double base = in.nextInt();
+
+        double area = (height * base)/2;
+
+        System.out.print("\nThe area of triangle with a ");
+        System.out.printf("%.1f", height);
+        System.out.print( " and ");
+        System.out.printf("%.1f", base);
+        System.out.print(" is ");
+        System.out.printf("%.1f", area);
+        System.out.print(".");
+        in.close();
+    }
+}
